8b83c6122591111bf73f461c92242ab7d0886196
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2026-04-02 15:44:57 +0800

    Fix wrapping issue for long fields

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -129,6 +129,11 @@ public class MainWindow extends UiPart<Stage> {
     private void setWindowDefaultSize(GuiSettings guiSettings) {
         primaryStage.setHeight(guiSettings.getWindowHeight());
         primaryStage.setWidth(guiSettings.getWindowWidth());
+        
+        // Set minimum window size to prevent shrinking below 1500px width
+        primaryStage.setMinWidth(1100);
+        primaryStage.setMinHeight(600);
+        
         if (guiSettings.getWindowCoordinates() != null) {
             primaryStage.setX(guiSettings.getWindowCoordinates().getX());
             primaryStage.setY(guiSettings.getWindowCoordinates().getY());
