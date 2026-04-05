00e3ba14c28249e61e85cd9388c2066dff4b656f
Depsheng <jiesheng.yeo@gmail.com>
2026-04-05 17:30:46 +0800

    Fix clear command parsing
    App now rejects user inputs with clear + parameters and only accepts clear by itself as an input

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -46,7 +46,9 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_clear() throws Exception {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
-        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
+        assertThrows(ParseException.class,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClearCommand.MESSAGE_USAGE), ()
+                    -> parser.parseCommand(ClearCommand.COMMAND_WORD + " 3"));
     }
 
     @Test
